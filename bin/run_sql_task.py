239bab88b7aa429ed939d5af427543d0fb5bea17
Marcos Prieto <marcospri@gmail.com>
2022-10-27 12:35:22 +0200

    Finally remove pyramid

--- bin/run_sql_task.py
+++ bin/run_sql_task.py
@@ -6,13 +6,12 @@ currently only used to perform the aggregations and mappings required for
 reporting.
 """
 
-
+import os
 from argparse import ArgumentParser
 
 import importlib_resources
 import sqlalchemy
 from psycopg2.extensions import parse_dsn
-from pyramid.paster import bootstrap
 
 from report.sql_tasks.sql_script import SQLScript
 
@@ -21,38 +20,30 @@ TASK_ROOT = importlib_resources.files("report.sql_tasks") / "tasks"
 parser = ArgumentParser(
     description=f"A script for running SQL tasks defined in: {TASK_ROOT}"
 )
-parser.add_argument(
-    "-c",
-    "--config-file",
-    required=True,
-    help="The paster config for this application. (e.g. development.ini)",
-)
-
 parser.add_argument("-t", "--task", required=True, help="The SQL task name to run")
 
 
 def main():
     args = parser.parse_args()
 
-    with bootstrap(args.config_file) as env:
-        dsn = env["registry"].settings["sqlalchemy.url"].strip()
+    dsn = os.environ["DATABASE_URL"].strip()
 
-        scripts = SQLScript.from_dir(
-            task_dir=TASK_ROOT / args.task,
-            template_vars={"db_user": parse_dsn(dsn)["user"]},
-        )
+    scripts = SQLScript.from_dir(
+        task_dir=TASK_ROOT / args.task,
+        template_vars={"db_user": parse_dsn(dsn)["user"]},
+    )
 
-        engine = sqlalchemy.create_engine(dsn)
+    engine = sqlalchemy.create_engine(dsn)
 
-        # Run the update in a transaction, so we roll back if it goes wrong
-        with engine.connect() as connection:
-            with connection.begin():
-                for script in scripts:
-                    print(f"Executing: {script.path}")
+    # Run the update in a transaction, so we roll back if it goes wrong
+    with engine.connect() as connection:
+        with connection.begin():
+            for script in scripts:
+                print(f"Executing: {script.path}")
 
-                    for query in script.queries:
-                        query.execute(connection)
-                        print(query.dump(indent="    ") + "\n")
+                for query in script.queries:
+                    query.execute(connection)
+                    print(query.dump(indent="    ") + "\n")
 
 
 if __name__ == "__main__":
