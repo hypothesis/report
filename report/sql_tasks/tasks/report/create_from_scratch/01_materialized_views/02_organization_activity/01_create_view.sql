21466d3a15d819c106299b8d2b6faffd9683456c
Jon Betts <jon.betts@hypothes.is>
2022-11-02 16:25:19 +0000

    Create a single key to use in metabase instead of a compound key
    Compounding seems to cause horrible trouble

--- report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/01_create_view.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/01_create_view.sql
@@ -1,9 +1,25 @@
 DROP MATERIALIZED VIEW IF EXISTS organization_activity CASCADE;
 
 CREATE MATERIALIZED VIEW organization_activity AS (
-    SELECT 'us' AS region, * FROM lms_us.organization_activity
+    SELECT
+        timescale,
+        period,
+        role,
+        CONCAT('us-', organization_id) AS organization_id,
+        annotation_count,
+        active,
+        billable
+    FROM lms_us.organization_activity
 
     UNION ALL
 
-    SELECT 'ca' AS region, * FROM lms_ca.organization_activity
+    SELECT
+        timescale,
+        period,
+        role,
+        CONCAT('ca-', organization_id) AS organization_id,
+        annotation_count,
+        active,
+        billable
+    FROM lms_ca.organization_activity
 ) WITH NO DATA;
