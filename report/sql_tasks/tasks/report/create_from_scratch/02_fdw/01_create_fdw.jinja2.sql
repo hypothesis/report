f7bc4924aeba9e0315b1ca0952b32b60074cdc4f
Marcos Prieto <marcospri@gmail.com>
2022-11-02 12:29:09 +0100

    Revert "Remove canadian connection for the first run"
    This reverts commit 71cd37c0115e6440f442bd84b8a9fa216450e49b.

--- report/sql_tasks/tasks/report/create_from_scratch/02_fdw/01_create_fdw.jinja2.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/02_fdw/01_create_fdw.jinja2.sql
@@ -7,9 +7,18 @@ DROP SERVER IF EXISTS "h_us_server" CASCADE;
 CREATE SERVER "h_us_server" FOREIGN DATA WRAPPER postgres_fdw
     OPTIONS (host '{{fdw.h_us.host}}', port '{{fdw.h_us.port}}', dbname '{{fdw.h_us.dbname}}');
 
+DROP SERVER IF EXISTS "h_ca_server" CASCADE;
+CREATE SERVER "h_ca_server" FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host '{{fdw.h_ca.host}}', port '{{fdw.h_ca.port}}', dbname '{{fdw.h_ca.dbname}}');
+
 DROP SERVER IF EXISTS "lms_us_server" CASCADE;
 CREATE SERVER "lms_us_server" FOREIGN DATA WRAPPER postgres_fdw
     OPTIONS (host '{{fdw.lms_us.host}}', port '{{fdw.lms_us.port}}', dbname '{{fdw.lms_us.dbname}}');
+
+DROP SERVER IF EXISTS "lms_ca_server" CASCADE;
+CREATE SERVER "lms_ca_server" FOREIGN DATA WRAPPER postgres_fdw
+    OPTIONS (host '{{fdw.lms_ca.host}}', port '{{fdw.lms_ca.port}}', dbname '{{fdw.lms_ca.dbname}}');
+
 
 DROP USER MAPPING IF EXISTS FOR "{{db_user}}" SERVER "h_us_server";
 CREATE USER MAPPING IF NOT EXISTS FOR "{{db_user}}"
@@ -19,10 +28,26 @@ CREATE USER MAPPING IF NOT EXISTS FOR "{{db_user}}"
         password '{{fdw.h_us.password}}' -- SECRET
     );
 
+DROP USER MAPPING IF EXISTS FOR "{{db_user}}" SERVER "h_ca_server";
+CREATE USER MAPPING IF NOT EXISTS FOR "{{db_user}}"
+    SERVER "h_ca_server"
+    OPTIONS (
+        user '{{fdw.h_ca.user}}',
+        password '{{fdw.h_ca.password}}' -- SECRET
+    );
+
 DROP USER MAPPING IF EXISTS FOR "{{db_user}}" SERVER "lms_us_server";
 CREATE USER MAPPING IF NOT EXISTS FOR "{{db_user}}"
     SERVER "lms_us_server"
     OPTIONS (
         user '{{fdw.lms_us.user}}',
         password '{{fdw.lms_us.password}}' -- SECRET
+    );
+
+DROP USER MAPPING IF EXISTS FOR "{{db_user}}" SERVER "lms_ca_server";
+CREATE USER MAPPING IF NOT EXISTS FOR "{{db_user}}"
+    SERVER "lms_ca_server"
+    OPTIONS (
+        user '{{fdw.lms_ca.user}}',
+        password '{{fdw.lms_ca.password}}' -- SECRET
     );
