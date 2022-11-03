7439434be6a676a1450c06a79644427c564a5609
Jon Betts <jon.betts@hypothes.is>
2022-11-03 16:24:30 +0000

    Support the new fields being calculated by LMS for organization activity

--- report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/01_create_view.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/01_create_view.sql
@@ -3,23 +3,31 @@ DROP MATERIALIZED VIEW IF EXISTS organization_activity CASCADE;
 CREATE MATERIALIZED VIEW organization_activity AS (
     SELECT
         timescale,
+        timestamp,
         period,
         role,
         CONCAT('us-', organization_id) AS organization_id,
         annotation_count,
+        annotation_count_growth,
         active,
-        billable
+        active_growth,
+        billable,
+        billable_growth
     FROM lms_us.organization_activity
 
     UNION ALL
 
     SELECT
         timescale,
+        timestamp,
         period,
         role,
         CONCAT('ca-', organization_id) AS organization_id,
         annotation_count,
+        annotation_count_growth,
         active,
-        billable
+        active_growth,
+        billable,
+        billable_growth
     FROM lms_ca.organization_activity
 ) WITH NO DATA;
