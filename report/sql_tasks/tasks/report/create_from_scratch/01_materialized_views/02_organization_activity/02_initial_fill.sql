408a78e228304731039a1df9726ae4e95199f558
Jon Betts <jon.betts@hypothes.is>
2022-11-08 15:23:31 +0000

    Pass on the calendar date for the organization activity report

--- report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/02_initial_fill.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/01_materialized_views/02_organization_activity/02_initial_fill.sql
@@ -1,4 +1,5 @@
 DROP INDEX IF EXISTS organization_activity_region_timescale_created_org_id_idx;
+DROP INDEX IF EXISTS organization_activity_calendar_date_idx;
 
 REFRESH MATERIALIZED VIEW organization_activity;
 
@@ -6,3 +7,4 @@ ANALYSE organization_activity;
 
 -- A unique index is mandatory for concurrent updates used in the refresh
 CREATE UNIQUE INDEX organization_activity_region_timescale_created_org_id_idx ON organization_activity (timescale, period, role, organization_id);
+CREATE INDEX organization_activity_calendar_date_idx ON organization_activity (calendar_date);
