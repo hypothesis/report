ede179be38d315b4fd17f114b0eedc6a171b3393
Marcos Prieto <marcospri@gmail.com>
2022-11-08 18:52:43 +0100

    Placeholder script to import hubspot data

---
+++ bin/import_hubspot.py
@@ -0,0 +1,14 @@
+"""Placeholder script for the hubspot data import script."""
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description="A script for importing hubspot data")
+
+
+def main():
+    _ = parser.parse_args()
+    print("Job done")
+
+
+if __name__ == "__main__":
+    main()
