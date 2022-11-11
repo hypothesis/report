841c57baab1ef3e6d18e8c8e8de5c2d9c0f4081b
Jon Betts <jon.betts@hypothes.is>
2022-11-11 20:14:35 +0000

    Add a "paying" column to the organization activity table based on HS data

--- report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/02_organization_activity/01_create_view.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/02_organization_activity/01_create_view.sql
@@ -1,27 +1,52 @@
 DROP MATERIALIZED VIEW IF EXISTS organization_activity CASCADE;
 
 CREATE MATERIALIZED VIEW organization_activity AS (
-    SELECT
-        timescale,
-        calendar_date,
-        period,
-        role,
-        CONCAT('us-', organization_id) AS organization_id,
-        annotation_count,
-        active,
-        billable
-    FROM lms_us.organization_activity
+    WITH
+        raw_organization_activity AS (
+            SELECT
+                timescale,
+                calendar_date,
+                period,
+                role,
+                CONCAT('us-', organization_id) AS organization_id,
+                annotation_count,
+                active,
+                billable
+            FROM lms_us.organization_activity
 
-    UNION ALL
+            UNION ALL
+
+            SELECT
+                timescale,
+                calendar_date,
+                period,
+                role,
+                CONCAT('ca-', organization_id) AS organization_id,
+                annotation_count,
+                active,
+                billable
+            FROM lms_ca.organization_activity
+        )
 
     SELECT
-        timescale,
-        calendar_date,
-        period,
-        role,
-        CONCAT('ca-', organization_id) AS organization_id,
-        annotation_count,
-        active,
-        billable
-    FROM lms_ca.organization_activity
+        raw_organization_activity.*,
+        COALESCE(paying, FALSE) AS paying
+    FROM raw_organization_activity
+    LEFT OUTER JOIN LATERAL (
+        SELECT
+            TRUE AS paying
+        FROM hubspot.deals
+        JOIN hubspot.company_deals ON
+            company_deals.deal_id = deals.id
+        JOIN hubspot.companies ON
+            companies.id = company_deals.company_id
+        JOIN organizations ON
+            organizations.public_id = companies.lms_organization_id
+        WHERE
+            organizations.id = organization_id
+            AND deals.services_start <= calendar_date
+            AND deals.services_end >= calendar_date
+            AND deals.amount > 0
+        LIMIT 1
+    ) AS paying ON TRUE
 ) WITH NO DATA;
