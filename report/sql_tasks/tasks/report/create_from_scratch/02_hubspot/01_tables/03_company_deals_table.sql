61485adf28ea9d19fccf9b363bfe5cf59eff3ce3
Jon Betts <jon.betts@hypothes.is>
2022-11-14 18:35:32 +0000

    Remove the company-deal index to investigate the data
    We don't exactly understand why the data is the way it is because we can't
    see it. This will allow us to investigate it and work out if we should
    dedupe, or if we are doing something wrong.

--- report/sql_tasks/tasks/report/create_from_scratch/02_hubspot/01_tables/03_company_deals_table.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/02_hubspot/01_tables/03_company_deals_table.sql
@@ -4,5 +4,3 @@ CREATE TABLE hubspot.company_deals (
     company_id BIGINT NOT NULL,
     deal_id BIGINT NOT NULL
 );
-
-CREATE UNIQUE INDEX company_deals_company_id_deal_id_idx ON hubspot.company_deals (company_id, deal_id);
