3c720b1c385a1804b3b4a43581291adc88c5856d
Jon Betts <jon.betts@hypothes.is>
2022-11-14 14:06:00 +0000

    Add the Hubspot company names to the organization
    Our names are quite sketchy. It's handy to be able to see the names
    we have via Hubspot.

--- report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/01_organization/01_create_view.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/01_organization/01_create_view.sql
@@ -1,25 +1,44 @@
 DROP MATERIALIZED VIEW IF EXISTS organizations CASCADE;
 
 CREATE MATERIALIZED VIEW organizations AS (
-    SELECT
-        CONCAT('us-', id) AS id,
-        CONCAT('us.lms.org.', public_id) AS public_id,
-        name,
-        'us' AS region,
-        created,
-        updated,
-        enabled
-    FROM lms_us.organization
+    WITH
+        raw_organizations AS (
+            SELECT
+                CONCAT('us-', id) AS id,
+                CONCAT('us.lms.org.', public_id) AS public_id,
+                name,
+                'us' AS region,
+                created,
+                updated,
+                enabled
+            FROM lms_us.organization
 
-    UNION ALL
+            UNION ALL
+
+            SELECT
+                CONCAT('ca-', id) AS id,
+                CONCAT('ca.lms.org.', public_id) AS public_id,
+                name,
+                'ca' AS region,
+                created,
+                updated,
+                enabled
+            FROM lms_ca.organization
+        ),
+
+        hubspot_names AS (
+            SELECT
+                lms_organization_id,
+                STRING_AGG(' / ', name) as hubspot_name
+            FROM hubspot.companies
+            WHERE lms_organization_id IS NOT NULL
+            GROUP BY lms_organization_id
+        )
 
     SELECT
-        CONCAT('ca-', id) AS id,
-        CONCAT('ca.lms.org.', public_id) AS public_id,
-        name,
-        'ca' AS region,
-        created,
-        updated,
-        enabled
-    FROM lms_ca.organization
+        raw_organizations.*,
+        hubspot_names.hubspot_name
+    FROM raw_organizations
+    JOIN hubspot_names ON
+        raw_organizations.public_id = hubspot_names.lms_organization_id
 ) WITH NO DATA;
