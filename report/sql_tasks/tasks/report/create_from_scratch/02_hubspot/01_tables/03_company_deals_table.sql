a8156c29acbe004c050f8c4c770e45d0c443ae43
Jon Betts <jon.betts@hypothes.is>
2022-11-15 13:42:00 +0000

    Re-add index to the company deals table now rows are unique

--- report/sql_tasks/tasks/report/create_from_scratch/02_hubspot/01_tables/03_company_deals_table.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/02_hubspot/01_tables/03_company_deals_table.sql
@@ -4,3 +4,5 @@ CREATE TABLE hubspot.company_deals (
     company_id BIGINT NOT NULL,
     deal_id BIGINT NOT NULL
 );
+
+CREATE UNIQUE INDEX company_deals_company_id_deal_id_idx ON hubspot.company_deals (company_id, deal_id);
