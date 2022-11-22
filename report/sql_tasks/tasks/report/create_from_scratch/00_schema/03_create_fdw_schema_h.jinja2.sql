1bd940c5f7dac1391d363c08bc503748b4f1fb80
Jon Betts <jon.betts@hypothes.is>
2022-11-22 19:07:37 +0000

    Aggregate and pass through user counts in authority activity from H

--- report/sql_tasks/tasks/report/create_from_scratch/00_schema/03_create_fdw_schema_h.jinja2.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/00_schema/03_create_fdw_schema_h.jinja2.sql
@@ -1,7 +1,19 @@
+-- Create types required for imports
+
+DROP TYPE IF EXISTS report.timescale CASCADE;
+
+CREATE TYPE report.timescale AS ENUM (
+    'week', 'month', 'semester', 'academic_year', 'year', 'all_time'
+);
+
 {% macro import_h(server_name, schema_name) %}
     DROP SCHEMA IF EXISTS {{schema_name}} CASCADE;
 
     CREATE SCHEMA {{schema_name}} AUTHORIZATION "{{db_user}}";
+
+    IMPORT FOREIGN SCHEMA "report" LIMIT TO (
+        authority_activity
+    ) FROM SERVER "{{server_name}}" INTO {{schema_name}};
 {% endmacro %}
 
 {{ import_h("h_us_server", "h_us") }}
