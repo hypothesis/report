53da87bce1b599656b944617e76903c096a7a44b
Jon Betts <jon.betts@hypothes.is>
2022-11-22 19:07:37 +0000

    Aggregate and pass through user counts in authority activity from H

--- report/sql_tasks/tasks/report/refresh/02_public/01_materialized_views_refresh.sql
+++ report/sql_tasks/tasks/report/refresh/02_public/01_materialized_views_refresh.sql
@@ -3,3 +3,6 @@ ANALYSE organizations;
 
 REFRESH MATERIALIZED VIEW CONCURRENTLY organization_activity;
 ANALYSE organization_activity;
+
+REFRESH MATERIALIZED VIEW CONCURRENTLY authority_activity;
+ANALYSE authority_activity;
