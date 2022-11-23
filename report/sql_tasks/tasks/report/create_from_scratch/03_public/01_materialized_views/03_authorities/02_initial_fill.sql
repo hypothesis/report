6b05f2855b17c993e588c5dc72f066d10bc78e33
Jon Betts <jon.betts@hypothes.is>
2022-11-23 20:11:59 +0000

    Pass through authorities

--- report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/03_authorities/02_initial_fill.sql
+++ report/sql_tasks/tasks/report/create_from_scratch/03_public/01_materialized_views/03_authorities/02_initial_fill.sql
@@ -1,10 +1,8 @@
-DROP INDEX IF EXISTS authority_activity_timescale_period_authority_id_region_idx;
-DROP INDEX IF EXISTS authority_activity_start_date_end_date_idx;
+DROP INDEX IF EXISTS authorities_id_idx;
 
-REFRESH MATERIALIZED VIEW authority_activity;
+REFRESH MATERIALIZED VIEW authorities;
 
-ANALYSE authority_activity;
+ANALYSE authorities;
 
 -- A unique index is mandatory for concurrent updates used in the refresh
-CREATE UNIQUE INDEX authority_activity_timescale_period_authority_id_region_idx ON authority_activity (timescale, period, authority_id, region);
-CREATE INDEX authority_activity_start_date_end_date_idx ON authority_activity (start_date, end_date);
+CREATE UNIQUE INDEX authorities_id_idx ON authorities (id);
