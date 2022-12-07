7e9acf7eb2d251caf257664588aadf98a1a1cb85
Jon Betts <jon.betts@hypothes.is>
2022-12-07 20:12:45 +0000

    Create new schema and create thin views in them with the new structure
    This introduces a new `h` and `lms` schema and places views in them which
    point back to the real views in `public`. Hopefully this will allow us to
    move over the reports in Metabase more gracefully and then move the views
    for real later.

--- report/data_tasks/report/create_from_scratch/00_schema/01_create_schemas.jinja2.sql
+++ report/data_tasks/report/create_from_scratch/00_schema/01_create_schemas.jinja2.sql
@@ -3,5 +3,14 @@
 DROP SCHEMA IF EXISTS report CASCADE;
 CREATE SCHEMA report AUTHORIZATION "{{db_user}}";
 
+-- Schema for storing data from Hubspot
 DROP SCHEMA IF EXISTS hubspot CASCADE;
 CREATE SCHEMA hubspot AUTHORIZATION "{{db_user}}";
+
+-- Schema for views and reports across all H instances
+DROP SCHEMA IF EXISTS h CASCADE;
+CREATE SCHEMA h AUTHORIZATION "{{db_user}}";
+
+-- Schema for views and reports across all LMS instances
+DROP SCHEMA IF EXISTS lms CASCADE;
+CREATE SCHEMA lms AUTHORIZATION "{{db_user}}";
