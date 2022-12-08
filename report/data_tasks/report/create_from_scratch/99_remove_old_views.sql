0a702eb0d5d436d3f9c1f4c17f53b37403eb5856
Jon Betts <jon.betts@hypothes.is>
2022-12-08 16:52:38 +0000

    Remove tidy up script (not required after being run once)

--- report/data_tasks/report/create_from_scratch/99_remove_old_views.sql
+++
@@ -1,7 +0,0 @@
--- This can be deleted once it's run once
-
-DROP VIEW IF EXISTS public.authorities;
-DROP VIEW IF EXISTS public.authority_activity;
-DROP VIEW IF EXISTS public.organizations;
-DROP VIEW IF EXISTS public.organization_activity;
-DROP VIEW IF EXISTS public.lms_events;
