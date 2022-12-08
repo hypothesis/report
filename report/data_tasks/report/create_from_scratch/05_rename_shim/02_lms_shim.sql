924be93745953bc416e81e9c7c8c6cd22abe7972
Jon Betts <jon.betts@hypothes.is>
2022-12-08 14:20:55 +0000

    Fix some copy paste errors in the view shim for LMS

--- report/data_tasks/report/create_from_scratch/05_rename_shim/02_lms_shim.sql
+++ report/data_tasks/report/create_from_scratch/05_rename_shim/02_lms_shim.sql
@@ -1,7 +1,7 @@
 -- Create thin view shims to allow us to start moving reports in Metabase
 
 DROP VIEW IF EXISTS lms.organizations;
-CREATE VIEW lms.authorities AS (SELECT * FROM public.organizations);
+CREATE VIEW lms.organizations AS (SELECT * FROM public.organizations);
 
 DROP VIEW IF EXISTS lms.organization_activity;
 CREATE VIEW lms.organization_activity AS (SELECT * FROM public.organization_activity);
