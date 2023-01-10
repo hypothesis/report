c93aa80d24b4fe6eb265dae815c01d5960fbeb28
Jon Betts <jon.betts@hypothes.is>
2023-01-10 16:51:26 +0000

    Calculate the values for `organization_group` directly

--- report/data_tasks/report/create_from_scratch/04_lms/04_groups/04_organization_group/01_create_view.sql
+++ report/data_tasks/report/create_from_scratch/04_lms/04_groups/04_organization_group/01_create_view.sql
@@ -4,5 +4,25 @@ CREATE MATERIALIZED VIEW lms.organization_group AS (
     -- Use distinct here to simplify the model from LMS. We want one row per
     -- org, rather than each org / lms-group combo
     SELECT DISTINCT group_id, organization_id
-    FROM lms.group_map
+    FROM (
+        SELECT
+        CONCAT('us-', group_id) AS group_id,
+        CASE
+            WHEN organization_id IS NULL THEN NULL
+            ELSE CONCAT('us-', organization_id)
+        END AS organization_id
+    FROM
+        lms_us.group_map
+
+    UNION ALL
+
+    SELECT
+        CONCAT('ca-', group_id) AS group_id,
+        CASE
+            WHEN organization_id IS NULL THEN NULL
+            ELSE CONCAT('ca-', organization_id)
+        END AS organization_id
+    FROM
+        lms_ca.group_map
+    ) AS data
 ) WITH NO DATA;
