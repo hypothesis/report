3c56513bfe88d6af11131abbb72040b373e52c7b
Jon Betts <jon.betts@hypothes.is>
2023-01-10 16:47:49 +0000

    Remove `group_map` as we no longer use it directly

--- report/data_tasks/report/refresh/03_lms/01_materialized_views_refresh.sql
+++ report/data_tasks/report/refresh/03_lms/01_materialized_views_refresh.sql
@@ -10,9 +10,6 @@ ANALYSE lms.events;
 REFRESH MATERIALIZED VIEW CONCURRENTLY lms.groups;
 ANALYSE lms.groups;
 
-REFRESH MATERIALIZED VIEW CONCURRENTLY lms.group_map;
-ANALYSE lms.group_map;
-
 REFRESH MATERIALIZED VIEW CONCURRENTLY lms.group_bubbled_activity;
 ANALYSE lms.group_bubbled_activity;
 
