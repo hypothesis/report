a1f39411f1492a8aecfae271b701c631b495085d
Jon Betts <jon.betts@hypothes.is>
2023-01-16 17:30:11 +0000

    Revert "Add views for sensitive user data and mappings to groups and organizations"
    This reverts commit 11a72be5e53bb371b0da151ee05a368d85df2af7.

--- report/data_tasks/report/create_from_scratch/04_lms/05_users/02_organization_roles/02_initial_fill.sql
+++
@@ -1,8 +0,0 @@
-DROP INDEX IF EXISTS lms.organization_roles_user_id_organization_id_role_idx;
-
-REFRESH MATERIALIZED VIEW lms.organization_roles;
-
-ANALYSE lms.organization_roles;
-
--- A unique index is mandatory for concurrent updates used in the refresh
-CREATE UNIQUE INDEX organization_roles_user_id_organization_id_role_idx ON lms.organization_roles (user_id, organization_id, role);
