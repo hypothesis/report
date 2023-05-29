ef50fff07833e57649a75fbaceefebee4bf910d2
Jon Betts <jon.betts@hypothes.is>
2023-05-29 18:20:49 +0100

    Use the methods in the existing Hubspot scripts

--- report/data_tasks/report/create_from_scratch/99_hubspot_write/01_company_property_update/02_hubspot_export.py
+++ report/data_tasks/report/create_from_scratch/99_hubspot_write/01_company_property_update/02_hubspot_export.py
@@ -1,46 +1,22 @@
-import csv
 import os
-import warnings
-from tempfile import NamedTemporaryFile
-
-from data_tasks.sql_query import SQLQuery
 
 from report.data_sources.hubspot_client import HubspotClient
+from report.data_sources.hubspot_sql import export_from_table
 
 
 def main(connection, **kwargs):
     """
-    Write our LMS properties back to Hubspot.
+    Write properties back to Hubspot for companies.
 
     This will populate various properties in the left hand sidebar of the
     company view with values defined by the view `company_property_update`.
     """
     api_client = HubspotClient(private_app_key=os.environ["HUBSPOT_API_KEY"])
 
-    # Read the view. We will insert this verbatim
-    query = SQLQuery(0, "SELECT * FROM hubspot.company_property_update")
-    query.execute(connection)
-    print(query.dump())
-
-    if not query.rows:
-        warnings.warn("No rows to export to Hubspot!")
-        return
-
-    with NamedTemporaryFile(mode="w", suffix=".csv") as csv_file:
-        print(f"Writing final CSV to {csv_file.name}...")
-        writer = csv.writer(csv_file)
-        writer.writerow(query.columns)
-
-        for row in query.rows:
-            writer.writerow(row)
-
-        # Ensure all rows are written to disk before we start to upload
-        csv_file.flush()
-
-        api_client.import_csv(
-            job_name="Report Export",
-            csv_files=[csv_file.name],
-            object_type=HubspotClient.ObjectType.COMPANY,
-        )
-
-        print("Uploaded to Hubspot")
+    export_from_table(
+        connection=connection,
+        api_client=api_client,
+        table_name="hubspot.company_property_update",
+        object_type=HubspotClient.ObjectType.COMPANY,
+        hubspot_job_name="Report Company Export",
+    )
