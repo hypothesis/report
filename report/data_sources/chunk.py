aa7c8cb6cdbb26bc8cc8f9cbd9cf3a7abc22587d
Jon Betts <jon.betts@hypothes.is>
2023-06-01 16:16:10 +0100

    Use a search based method to retrieve relevant contacts from HS
    This means we find the contacts we need to update, rather than all
    of them. This should be much faster (2 minutes vs 35).
    
    This does mean we cannot update people who we no longer match.

--- report/data_sources/chunk.py
+++ report/data_sources/chunk.py
@@ -19,3 +19,32 @@ def chunk(items: Iterable, chunk_size: int):
     # cause iter to call the lambda until it returns an empty tuple, which
     # should consume the iterable above.
     return iter(lambda: tuple(islice(iter_items, chunk_size)), ())
+
+
+def chunk_with_max_len(items: Iterable[str], chunk_size: int, max_chars: int):
+    """Return items in chunks with a maximum size and char length.
+
+    :param items: Items to break into chunks
+    :param max_chars: Maximum total char length of a single chunk
+    :param chunk_size: The maximum size of a chunk
+
+    :raises ValueError: If `chunk_size` or `max_chars` is not a positive integer
+    """
+    if chunk_size < 1:
+        raise ValueError(f"Invalid chunk size: {chunk_size}")
+
+    if max_chars < 1:
+        raise ValueError(f"Invalid max chars: {max_chars}")
+
+    batch, batch_chars = [], 0
+
+    for item in items:
+        batch_chars += len(item)
+        if batch and batch_chars >= max_chars or len(batch) >= chunk_size:
+            yield batch
+            batch, batch_chars = [], 0
+
+        batch.append(item)
+
+    if batch:
+        yield batch
