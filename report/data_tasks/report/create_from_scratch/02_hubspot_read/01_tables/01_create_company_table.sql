7bee61229e8a822f146f80976b0aedfec0d8ae78
Jon Betts <jon.betts@hypothes.is>
2023-07-05 19:36:49 +0100

    Add `is_deal` and `life_cycle_stage` to companies

--- report/data_tasks/report/create_from_scratch/02_hubspot_read/01_tables/01_create_company_table.sql
+++ report/data_tasks/report/create_from_scratch/02_hubspot_read/01_tables/01_create_company_table.sql
@@ -1,10 +1,13 @@
+-- Once this has run once, it can be removed
 DROP TABLE IF EXISTS hubspot.companies CASCADE;
+DROP TABLE IF EXISTS hubspot.companies_raw CASCADE;
 
-CREATE TABLE hubspot.companies (
+CREATE TABLE hubspot.companies_raw (
    -- General
     id BIGINT PRIMARY KEY,
     name TEXT,
     lms_organization_id TEXT,
+    life_cycle_stage TEXT,
     -- Owners
     company_owner_id INT,
     success_owner_id INT,
@@ -19,4 +22,24 @@ CREATE TABLE hubspot.companies (
     deals_last_update DATE
 );
 
-CREATE INDEX companies_lms_organization_id_idx ON hubspot.companies (lms_organization_id);
+CREATE INDEX companies_lms_organization_id_idx ON hubspot.companies_raw (lms_organization_id);
+
+-- If we were using Postgres 12+ we could use a calculated column. This is the
+-- next best thing where we have a view with a calculated column. I'm hoping we
+-- can get away without materializing this.
+DROP VIEW IF EXISTS hubspot.companies CASCADE;
+
+CREATE VIEW hubspot.companies AS (
+    SELECT
+        *,
+        CASE
+            WHEN
+                current_deal_services_start IS NOT NULL
+                AND current_deal_services_start <= NOW()
+                AND current_deal_services_end IS NOT NULL
+                AND current_deal_services_end >= NOW()
+            THEN True
+            ELSE False
+        END AS in_deal
+    FROM hubspot.companies_raw
+);
